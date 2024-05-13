d245dd120d2586ea0d532497c1d02740c6d44e68
mahdivedad <157973078+mahdivedad@users.noreply.github.com>
2024-05-13 22:10:35 +0330

    test python File added

---
+++ test.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, We are Mahdi Vedad Mahmooudi,Seyed Mohammad Hossein Hosseini and Behrad Ghasemi in Programmer Scientists to code TaskNevis project!</p>"
