82d992c9e85aa93392ebbb7379edbe5ac1ab5a73
mahdi vedad mahmooudi <mahdivedad1384@gmail.com>
2024-06-19 21:19:34 +0330

    Delete Task page added and Task adding page controllers added

--- app.py
+++ app.py
@@ -49,34 +49,34 @@ class TASK(Base):
     
     
 
-    def __repr__(self):
-         html= f"""
-         <table border="1">
-              <tr>
-                  <th>Task</th>
-                  <th>Description</th>
-                  <th>Owner</th>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Condition</th>
-              </tr>
-              <tr>
-                  <td>{self.task}</td>
-                  <td>{self.description}</td>
-                  <td>{self.owner}</td>
-                  <td>{self.Date}</td>
-                  <td>{self.Time}</td>
-                  <td>{self.Condition}</td>
-              </tr>
-         </table>
-         """
-         return (html)
+    # def __repr__(self):
+    #      html= f"""
+    #      <table border="1">
+    #           <tr>
+    #               <th>Task</th>
+    #               <th>Description</th>
+    #               <th>Owner</th>
+    #               <th>Date</th>
+    #               <th>Time</th>
+    #               <th>Condition</th>
+    #           </tr>
+    #           <tr>
+    #               <td>{self.task}</td>
+    #               <td>{self.description}</td>
+    #               <td>{self.owner}</td>
+    #               <td>{self.Date}</td>
+    #               <td>{self.Time}</td>
+    #               <td>{self.Condition}</td>
+    #           </tr>
+    #      </table>
+    #      """
+    #      return (html)
 
   
          
 
     # def __repr__(self):
-    #      return f"[{self.task}{self.description} {self.owner} {self.Date} {self.Time} {self.Condition}]"
+    #       return f"({self.task}{self.description} {self.owner} {self.Date} {self.Time} {self.Condition})"
      
     
 engine = create_engine("sqlite:///users.db", echo=True)
@@ -95,12 +95,29 @@ def validuser(input):
         return True
     return False
 
+def ValidTaskAdd(task,owner):
+    result = session.query(TASK).filter(TASK.task == task and TASK.owner == owner)
+    for r in result:
+        return True
+    return False
+
 def validlogin(username , password):
     result = session.query(USER).filter(USER.username == username , USER.password == password)
     for r in result:
         return True
     return False
 
+def validEdit(username , Taskname):
+    result= session.query(TASK).filter(TASK.owner == username , TASK.task == Taskname)
+    for r in result:
+        return True
+    return False
+
+def anyData(TaskName,username):
+    result= session.query(TASK).filter(TASK.owner == username , TASK.task == TaskName)
+    for r in result:
+        return True
+    return False
 
 @app.route("/")
 def index():
@@ -151,6 +168,10 @@ def mainpage():
             return render_template("Task.html", username = username)
         elif action == "change":
             return render_template("changepassword.html", username = username)
+        elif action == "editTask":
+            return render_template("EditTasks.html", username = username)
+        elif action == "deleteTask":
+            return render_template("DeleteTask.html", username = username)
         else:
             return render_template("mainpage.html", username = username)
     return redirect("/")
@@ -178,18 +199,25 @@ def Task():
     if request.method == "POST":
          task=request.form.get("TaskName")
          description=request.form.get("Task")
-         owner=request.form.get("Username")
+         owner = request.form.get("Username")
          Date=request.form.get("DateInput")
          Time=request.form.get("TimeInput")
          Condition=request.form.get("condition")
-
+    if ValidTaskAdd(task,owner):
+         return render_template("Task.html" , invalid = "There is a Task with this name. please set another name for your new task")  
+    else:
+        task = TASK(task, description, owner, Date, Time, Condition)
+        session.add(task)
+        session.commit()
+        return render_template("mainpage.html",username = owner)
+       
+     
+@app.route("/Backtomainpage" , methods = ["POST","GET"])
+def BacktoMainPage():
+    if request.method == "POST":
+        mainpageusername = request.form.get("username")
+        return render_template("mainpage.html",username = mainpageusername)   
          
-    task = TASK(task, description, owner, Date, Time, Condition)
-    session.add(task)
-    session.commit()
-    return ("Done")
-
-
 
 @app.route("/ShowTask", methods=["POST","GET"])
 def ShowTask():
@@ -198,6 +226,41 @@ def ShowTask():
         result = session.query(TASK).filter(TASK.owner == ShowTask).all()
         result =" ".join(map(str, result))
         return render_template("ShowTask.html", rows=result,username=ShowTask)
+    
+
+@app.route("/deleteTask", methods=["POST","GET"])
+def deleteTask():
+    if request.method == "POST":
+        username = request.form.get("username")
+        TaskName = request.form.get("taskname")
+        if anyData(TaskName,username):
+            result = session.query(TASK).filter(TASK.owner == username , TASK.task == TaskName).first()
+            session.delete(result)
+            session.commit()
+            return render_template("DeleteTask.html", username = username , invalid="Task deleted successfully")
+        else:
+            return render_template("DeleteTask.html", username = username,invalid="There is no Task with this name")
+           
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
+# @app.route("/EditTask",methods=["POST",'GET'])
+# def EditTask():
+#     username = request.form.get("username")
+#     if request.method == "POST":
+#         Taskname=request.form.get("NameTask")
+#         Task=request.form.get("Task")
+#         Condition=request.form.get("Condition")
+#         Username=request.form.get("username")
+#         if validEdit(Username , Taskname):
+#             Task = session.query(TASK).filter(TASK.task == Taskname).first()
+#             Task.task = Taskname
+#             Task.description = Task
+#             Task.Date = None
+#             Task.Time = None
+#             Task.Condition = Condition
+#             db.session.commit()
+#             return redirect(url_for("mainpage"))
+#     return render_template("EditTasks.html", username = username)
